632250f3153985d31b3df3e5dca80cedd7b8cb07
AlexejSem <aleksejsemaskevic0@gmail.com>
2022-08-23 12:12:03 +0300

    task_20 refactoring code in StaticProvider.java

--- src/test/java/StaticProvider.java
+++ src/test/java/StaticProvider.java
@@ -4,11 +4,11 @@ public class StaticProvider {
     @DataProvider(name = "path",parallel = true)
     public static Object[][] createData() {
         return new Object[][]{
-                new Object[]{new String(" ")}
-                ,{("src/main/resources/alexey-cart.json")}
-                ,{("src/main/resources/andrey-cart.json")}
-                ,{("src/main/resources/evgen-cart.json")}
-                ,{("test")}
+                {" "},
+                {"src/main/resources/alexey-cart.json"},
+                {"src/main/resources/andrey-cart.json"},
+                {"src/main/resources/evgen-cart.json"},
+                {"test"}
         };
     }
 }
