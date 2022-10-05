50b36cc864a84d38e224f650d6993de5e4ae7350
AlexejSem <aleksejsemaskevic0@gmail.com>
2022-10-05 12:00:53 +0300

    task 60 - Page Object - common logic is removed to @BeforeEach

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.WebDriver;
@@ -9,8 +10,11 @@ public class LoginTest {
 
     private WebDriver webDriver;
     private StartPage startPage;
+    private LoginPage loginPage;
+    private MailInboxPage mailInboxPage;
     private final static String URL = ("https://mail.yandex.com/");
-    private static final String LOGIN_AND_PASSWORD = "task30, task30task30";
+    private static final String LOGIN = "task30";
+    private static final String PASSWORD = "task30task30";
     private static final String EXPECTED_TEXT = "inbox";
 
 
@@ -20,22 +24,20 @@ public class LoginTest {
         webDriver = browser.getWebDriver();
         webDriver.get(URL);
         startPage = new StartPage(webDriver);
+        loginPage = startPage.clickLoginButton();
+        mailInboxPage = loginPage.loginToMail(LOGIN, PASSWORD);
+
+
     }
 
-    @ParameterizedTest
-    @CsvSource(LOGIN_AND_PASSWORD)
-    void logoutFromYandexMailTest(String login, String password) {
-        LoginPage loginPage = startPage.clickLoginButton();
-        MailInboxPage mailInboxPage = loginPage.loginToMail(login, password);
+    @Test
+    void logoutFromYandexMailTest() {
         mailInboxPage.logout();
         Assertions.assertTrue(startPage.isLogInButtonDisplayed());
     }
 
-    @ParameterizedTest
-    @CsvSource(LOGIN_AND_PASSWORD)
-    void loginToYandexMailTest(String login, String password) {
-        LoginPage loginPage = startPage.clickLoginButton();
-        MailInboxPage mailInboxPage = loginPage.loginToMail(login, password);
+    @Test
+    void loginToYandexMailTest() {
         Assertions.assertEquals(mailInboxPage.getInboxText(), EXPECTED_TEXT);
     }
 
