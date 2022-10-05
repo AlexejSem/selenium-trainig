4341592a8fe5c132846adeeb870148f753feee1f
AlexejSem <aleksejsemaskevic0@gmail.com>
2022-10-05 19:43:13 +0300

    task 70

--- src/main/java/StartPage.java
+++ src/main/java/StartPage.java
@@ -1,17 +1,28 @@
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class StartPage {
 
     private WebDriver webDriver;
     private static By LOGIN_BUTTON = By.cssSelector("button[type]");
+    private static String SCREENSHOT_FILE_PATH = "src/test/recorces/screenshot.png";
 
     public StartPage(WebDriver webDriver) {
         this.webDriver = webDriver;
+    }
+
+    public void getScreenShot() throws IOException {
+        File scrFile = ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File(SCREENSHOT_FILE_PATH));
     }
 
     public LoginPage clickLoginButton() {
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,9 +2,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.WebDriver;
+
+import java.io.IOException;
 
 public class LoginTest {
 
@@ -18,11 +18,12 @@ public class LoginTest {
 
 
     @BeforeEach
-    void setup() {
+    void setup() throws IOException {
         Browser browser = Browser.getInstance();
         webDriver = browser.getWebDriver();
         webDriver.get(URL);
         startPage = new StartPage(webDriver);
+        startPage.getScreenShot();
         LoginPage loginPage = startPage.clickLoginButton();
         mailInboxPage = loginPage.loginToMail(LOGIN, PASSWORD);
 
